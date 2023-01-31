29e862082284f11c022994ede99ae282cb7f15a0
Jose Padilla <jp06171@gmail.com>
2023-01-31 16:16:54 -0600

    update

--- client/shared/context/users/users.ts
+++ client/shared/context/users/users.ts
@@ -4,6 +4,7 @@ import { User } from './types';
 export const me = async (): Promise<User> => {
   const { data } = await api.get('/users/me', {
     headers: {
+      'Access-Control-Allow-Origin': '*',
       Authorization: `Bearer ${localStorage.getItem('access_token')}`,
     },
   });
